d54f7ba624fa87ab50980d4a3890acd4ac72e868
emma.keyes <emma.keyes@CLVWKS160.CLAVIS.local>
2018-02-21 14:51:28 +0000

    Removal of unused accType setter method commit

--- src/bankexcercise/BankAccount.java
+++ src/bankexcercise/BankAccount.java
@@ -59,10 +59,6 @@ public class BankAccount {
 		this.accountType = accountType;
 	}
 	
-	public void setAccType(String accType){
-		accType = accType;
-	}
-	
 	public String getAccType(){
 		return accountType;
 	}
