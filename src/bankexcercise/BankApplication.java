5640a9222f78e6be92a52acc2fc2dae7151d5410
emma.keyes <emma.keyes@CLVWKS160.CLAVIS.local>
2018-02-21 18:50:27 +0000

    Error handling for invalid account number for withdraw action listener commit

--- src/bankexcercise/BankApplication.java
+++ src/bankexcercise/BankApplication.java
@@ -441,13 +441,15 @@ public class BankApplication extends JFrame {
 		transMenuItems.get("Withdraw").addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				String accNum = JOptionPane.showInputDialog("Account number to withdraw from: ");
-				String toWithdraw = JOptionPane.showInputDialog("Account found, Enter Amount to Withdraw: ");
+				//String toWithdraw = JOptionPane.showInputDialog("Account found, Enter Amount to Withdraw: ");
+				boolean found = false;
 
 				for (Map.Entry<Integer, BankAccount> entry : table.entrySet()) {
 
 
 					if(accNum.equals(entry.getValue().getAccountNumber().trim())){
-
+						String toWithdraw = JOptionPane.showInputDialog("Account found, Enter Amount to Withdraw: ");
+						found = true;
 						if(entry.getValue().getAccountType().trim().equals("Current")){
 							if(Double.parseDouble(toWithdraw) > entry.getValue().getBalance() + entry.getValue().getOverdraft())
 								JOptionPane.showMessageDialog(null, "Transaction exceeds overdraft limit");
@@ -466,6 +468,8 @@ public class BankApplication extends JFrame {
 						}
 					}					
 				}
+				if (!found)
+					JOptionPane.showMessageDialog(null, "Account number " + accNum + " not found.");
 			}
 		});
 
